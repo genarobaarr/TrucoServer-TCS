bf00ce10493655ffb557e47e0a28551b9da7f666
themrsomg <themrsomg117@gmail.com>
2025-10-15 22:39:06 -0600

    refactor: Corrección de lógica

--- TrucoServer/baseDatosTruco.Designer.cs
+++ TrucoServer/baseDatosTruco.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\genae\source\repos\TrucoServer\TrucoServer\baseDatosTruco.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\themr\Source\Repos\TrucoServer-TCS\TrucoServer\baseDatosTruco.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
