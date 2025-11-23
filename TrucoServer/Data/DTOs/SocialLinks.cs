2d551315031aa0e12a24bae99012a5afc037761f
themrsomg <themrsomg117@gmail.com>
2025-11-22 20:51:43 -0600

    fix: Corrección de funcionalidad de redes sociales del usuario

--- TrucoServer/Data/DTOs/SocialLinks.cs
+++ TrucoServer/Data/DTOs/SocialLinks.cs
@@ -1,9 +1,16 @@
-﻿namespace TrucoServer.Data.DTOs
+﻿using Newtonsoft.Json;
+
+namespace TrucoServer.Data.DTOs
 {
     public class SocialLinks
     {
+        [JsonProperty("facebook")]
         public string FacebookHandle { get; set; }
+
+        [JsonProperty("x")]
         public string XHandle { get; set; }
+
+        [JsonProperty("instagram")]
         public string InstagramHandle { get; set; }
     }
 }
