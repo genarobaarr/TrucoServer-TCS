70057cf1026722a7546069c2b6c5efb28a6792e6
themrsomg <themrsomg117@gmail.com>
2025-11-28 16:22:41 -0600

    tests: Reestructuración de tests

---
+++ TrucoServer.Tests/DataTests/EntititesTests/FriendShipMappingTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrucoServer.Tests.DataTests.EntititesTests
+{
+    internal class FriendShipMappingTests
+    {
+    }
+}
