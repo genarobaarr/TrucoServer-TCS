ee4d0d5fb581b28b945ae08e9b42617a9df96eaa
themrsomg <themrsomg117@gmail.com>
2025-11-28 16:43:38 -0600

    tests: Cambios en definiciones de clases de prueba

--- TrucoServer.Tests/HelpersTests/ProfilesTests/IProfileUpdaterTests.cs
+++ TrucoServer.Tests/HelpersTests/ProfilesTests/IProfileUpdaterTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace TrucoServer.Tests.HelpersTests.ProfilesTests
 {
-    internal class IProfileUpdaterTests
+    [TestClass]
+    public class IProfileUpdaterTests
     {
     }
 }
