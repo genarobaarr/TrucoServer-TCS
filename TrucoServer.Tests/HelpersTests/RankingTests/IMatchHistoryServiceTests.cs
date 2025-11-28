baaab98e08bbbc52a7ecda4712123d275c72e268
themrsomg <themrsomg117@gmail.com>
2025-11-28 16:24:43 -0600

    tests: Continuación de estrucutración de pruebas

---
+++ TrucoServer.Tests/HelpersTests/RankingTests/IMatchHistoryServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrucoServer.Tests.HelpersTests.RankingTests
+{
+    internal class IMatchHistoryServiceTests
+    {
+    }
+}
