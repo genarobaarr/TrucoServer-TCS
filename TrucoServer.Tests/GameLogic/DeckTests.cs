b354a8a5c154a044d65679982f0659024ebd6030
themrsomg <themrsomg117@gmail.com>
2025-11-28 16:50:35 -0600

    tests: Cambios en definiciones de clases de prueba

--- TrucoServer.Tests/GameLogic/DeckTests.cs
+++ TrucoServer.Tests/GameLogic/DeckTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace TrucoServer.Tests.GameLogic
 {
-    internal class DeckTests
+    [TestClass]
+    public class DeckTests
     {
     }
 }
