447b1f7b1c44be322cf0066f94add1ea048ff53b
themrsomg <themrsomg117@gmail.com>
2025-11-28 20:36:40 -0600

    tests: Se añadieron tests

--- TrucoServer.Tests/DataTests/EntititesTests/FriendShipMappingTests.cs
+++ TrucoServer.Tests/DataTests/EntititesTests/FriendShipMappingTests.cs
@@ -1,14 +1,61 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TrucoServer.Data.Entities;
 
 namespace TrucoServer.Tests.DataTests.EntititesTests
 {
     [TestClass]
     public class FriendShipMappingTests
     {
+        [TestMethod]
+        public void TestFriendshipClassShouldHaveMetadataTypeAttribute()
+        {
+            var type = typeof(Friendship);
+            var attribute = type.GetCustomAttribute<MetadataTypeAttribute>();
+            Assert.IsNotNull(attribute, "The Friendship partial class must have the MetadataType attribute.");
+        }
+
+        [TestMethod]
+        public void TestUserIDPropertyShouldHaveColumnAttributeUserID1()
+        {
+            var property = typeof(FriendShipMapping).GetProperty("UserID");
+            var columnAttr = property.GetCustomAttribute<ColumnAttribute>();
+            Assert.IsNotNull(columnAttr);
+            Assert.AreEqual("userID1", columnAttr.Name);
+        }
+
+        [TestMethod]
+        public void TestFriendIDPropertyShouldHaveColumnAttributeUserID2()
+        {
+            var property = typeof(FriendShipMapping).GetProperty("FriendID");
+            var columnAttr = property.GetCustomAttribute<ColumnAttribute>();
+            Assert.IsNotNull(columnAttr);
+            Assert.AreEqual("userID2", columnAttr.Name);
+        }
+
+        [TestMethod]
+        public void TestUserIDSetPropertyShouldStoreIntegerValue()
+        {
+            var mapping = new FriendShipMapping();
+            int expectedId = 100;
+            mapping.UserID = expectedId;
+            Assert.AreEqual(expectedId, mapping.UserID);
+        }
+
+        [TestMethod]
+        public void TestFriendIDSetNegativeValueShouldStoreValue()
+        {
+            var mapping = new FriendShipMapping();
+            int negativeId = -50;
+            mapping.FriendID = negativeId;
+            Assert.AreEqual(negativeId, mapping.FriendID);
+        }
     }
 }
