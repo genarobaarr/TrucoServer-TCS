725e9bde91e44fda59b24bd814c70b7e73237159
themrsomg <themrsomg117@gmail.com>
2025-11-28 21:30:55 -0600

    tests: Se añadieron tests

--- TrucoServer.Tests/UtilitiesTests/ConfigurationReaderTests.cs
+++ TrucoServer.Tests/UtilitiesTests/ConfigurationReaderTests.cs
@@ -1,14 +1,68 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TrucoServer.Utilities;
 
 namespace TrucoServer.Tests.UtilitiesTests
 {
     [TestClass]
     public class ConfigurationReaderTests
     {
+        private const string FILE_NAME = "appSettings.private.json";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var field = typeof(ConfigurationReader).GetField("emailSettings", BindingFlags.NonPublic | BindingFlags.Static);
+            field.SetValue(null, null);
+            
+            if (File.Exists(FILE_NAME))
+            {
+                File.Delete(FILE_NAME);
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (File.Exists(FILE_NAME)) File.Delete(FILE_NAME);
+        }
+
+        [TestMethod]
+        public void TestEmailSettingsValidJsonFileShouldLoadProperties()
+        {
+            string jsonContent = @"{
+            'EmailSettings': {
+                'FromAddress': 'test@truco.com',
+                'SmtpPort': 587
+            }
+        }";
+
+            File.WriteAllText(FILE_NAME, jsonContent);
+            var settings = ConfigurationReader.EmailSettings;
+            Assert.IsNotNull(settings);
+            Assert.AreEqual("test@truco.com", settings.FromAddress);
+            Assert.AreEqual(587, settings.SmtpPort);
+        }
+
+        [TestMethod]
+        public void TestEmailSettingsMissingFileShouldLogFatalAndNotThrow()
+        {
+            var settings = ConfigurationReader.EmailSettings;
+            Assert.IsNull(settings, "If file is missing, settings should be null as exception is caught.");
+        }
+
+        [TestMethod]
+        public void TestEmailSettingsMalformedJsonShouldLogFatalAndReturnNull()
+        {
+            File.WriteAllText(FILE_NAME, "{ Invalid Json }");
+            var settings = ConfigurationReader.EmailSettings;
+            Assert.IsNull(settings);
+        }
     }
 }
--- TrucoServer.Tests/UtilitiesTests/EmailSettingsTests.cs
+++ TrucoServer.Tests/UtilitiesTests/EmailSettingsTests.cs
@@ -4,11 +4,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrucoServer.Utilities;
 
 namespace TrucoServer.Tests.UtilitiesTests
 {
     [TestClass]
     public class EmailSettingsTests
     {
+        [TestMethod]
+        public void TestFromAddressPropertySetShouldStoreValue()
+        {
+            var settings = new EmailSettings();
+            string email = "test@domain.com";
+            settings.FromAddress = email;
+            Assert.AreEqual(email, settings.FromAddress);
+        }
+
+        [TestMethod]
+        public void TestSmtpPortPropertySetShouldStoreIntegerValue()
+        {
+            var settings = new EmailSettings();
+            int port = 587;
+            settings.SmtpPort = port;
+            Assert.AreEqual(port, settings.SmtpPort);
+        }
+
+        [TestMethod]
+        public void TestEnableSslPropertySetTrueShouldStoreTrue()
+        {
+            var settings = new EmailSettings();
+            settings.EnableSsl = true;
+            Assert.IsTrue(settings.EnableSsl);
+        }
+
+        [TestMethod]
+        public void TestFromPasswordPropertySetNullShouldStoreNull()
+        {
+            var settings = new EmailSettings();
+            settings.FromPassword = null;
+            Assert.IsNull(settings.FromPassword);
+        }
     }
 }
